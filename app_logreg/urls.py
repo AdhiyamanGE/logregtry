aefa7a657b9d24536a3aee19b3833b2c2cda6320
Kal-AD <adhiyamangnanajothi@gmail.com>
2023-02-09 23:28:56 +0530

    .

--- app_logreg/urls.py
+++ app_logreg/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
-    path('',views.home,name="blog-home"),
+    path('register',views.register,name="register"),
+    path('home',views.home,name="home"),
+    path('',auth_views.LoginView.as_view(template_name='app_logreg/login.html'),name="login"),
 ]
--- app_logreg/views.py
+++ app_logreg/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-def home(request):
+def register(request):
     if request.method=='POST':
         form=UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+        return redirect('login')
     else:
         form=UserRegisterForm()
-    return render(request,'app_logreg/nono.html',{'form':form})
+    return render(request,'app_logreg/register.html',{'form':form})
+
+@login_required
+def home(request):
+    return HttpResponse("<h1>home</h1>")
